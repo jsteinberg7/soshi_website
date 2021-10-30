6bc949881e852be74b50460e6e0dad7884d5606b
Jason <j.steinberg702@gmail.com>
2021-10-30 18:24:11 -0400

    cleaned up code a bit

--- lib/url.dart
+++ lib/url.dart
@@ -12,7 +12,9 @@ abstract class URL {
   // (Linkedin, Faceboook, etc. use a numbering system)
   static String getPlatformURL(
       {required String platform, required String username}) {
-    if (platform == "Instagram") {
+      if (platform == "Phone") {
+      return "sms://$username";
+    } else if (platform == "Instagram") {
       return "https://www.instagram.com/" + username + "/?hl=en";
     } else if (platform == "Snapchat") {
       return "https://www.snapchat.com/add/" + username;
@@ -28,8 +30,6 @@ abstract class URL {
       return username;
     } else if (platform == "Discord") {
       return "https://discordapp.com/users/" + username + "/";
-    } else if (platform == "Phone") {
-      return "sms://$username";
     }
     // to make compiler happy
     return "";
