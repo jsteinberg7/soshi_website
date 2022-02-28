7b1715b0c5639a7665cfaf2139f24d3d2746023f
jsteinberg7 <j.steinberg702@gmail.com>
2022-02-28 16:35:05 -0500

    fixed hash url bug

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:firebase_core/firebase_core.dart';
 
 import 'package:flutter/material.dart';
@@ -7,18 +9,22 @@ import 'package:soshi/page_not_found_screen.dart';
 import 'package:soshi/url.dart';
 import 'package:soshi/user.dart';
 import 'package:soshi/userinfodisplay.dart';
+import 'customUrlStrategy.dart';
 import 'database.dart';
 import 'loading_screen.dart';
-import 'package:url_strategy/url_strategy.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 void main() async {
-  // setPathUrlStrategy();
+  setUrlStrategy(PathUrlStrategy());
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(MyApp());
 }
 
 Future<User> fetchUserData(String soshiUsername) async {
+  var url = window.location.href;
+  print("URL: " + url);
+
   print("attempt to fetch user data base using $soshiUsername");
 
   DatabaseService databaseService =
@@ -69,42 +75,60 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
+    /* remove # if necessary */
+    print("URL: " + window.location.href);
+    List<String> inputURL = window.location.href.split("/");
+    if (inputURL.contains("#")) {
+      window.history.pushState(null, "User", "/user/${inputURL.last}");
+    }
+    // window.history.pushState(null, "Home", "/#/user/testing");
+    // print("pushing url");
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: Constants.CustomTheme,
         initialRoute: "/",
         onGenerateRoute: (settings) {
+          print("SETTINGS: " + settings.toString());
           List<String> params = settings.name!.split("/");
           String UID = params.last;
+          // if URL has slash at end, remove slash from UID
+          if (UID.endsWith('/')) {
+            UID = UID.replaceAll('/', '');
+          }
           // String UID = "yuvansun";
 
-          //if (params.contains("user")) {
-          if (true) {
-            return MaterialPageRoute(builder: (context) {
-              return FutureBuilder(
-                  future: fetchUserData(UID),
-                  builder: (BuildContext context, snapshot) {
-                    if (snapshot.connectionState == ConnectionState.done &&
-                        snapshot.hasData) {
-                      User user = snapshot.data as User;
-                      return UserInfoDisplay(
-                        fullName: user.fullName,
-                        usernames: user.usernames,
-                        visiblePlatforms: user.visiblePlatforms,
-                        photoURL: user.photoURL,
-                        bio: user.userBio,
-                        friendsAdded: user.friendsAdded,
-                        //soshiUsername: user.soshiUsername,
-                        //userBio: user.userBio
-                      );
-                    } else if (snapshot.connectionState ==
-                        ConnectionState.waiting) {
-                      return LoadingScreen();
-                    } else {
-                      return PageNotFoundScreen(launchURLIn: false);
-                    }
-                  });
-            });
+          if (params.length >= 2 &&
+              params[1] == "user" &&
+              params.last != "user") {
+            print("params:" + params.toString());
+            // if (true) {
+            return MaterialPageRoute(
+                settings: settings,
+                builder: (context) {
+                  return FutureBuilder(
+                      future: fetchUserData(UID),
+                      builder: (BuildContext context, snapshot) {
+                        if (snapshot.connectionState == ConnectionState.done &&
+                            snapshot.hasData) {
+                          User user = snapshot.data as User;
+                          return UserInfoDisplay(
+                            fullName: user.fullName,
+                            usernames: user.usernames,
+                            visiblePlatforms: user.visiblePlatforms,
+                            photoURL: user.photoURL,
+                            bio: user.userBio,
+                            friendsAdded: user.friendsAdded,
+                            //soshiUsername: user.soshiUsername,
+                            //userBio: user.userBio
+                          );
+                        } else if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return LoadingScreen();
+                        } else {
+                          return PageNotFoundScreen(launchURLIn: false);
+                        }
+                      });
+                });
           } else {
             return MaterialPageRoute(builder: (context) {
               return PageNotFoundScreen(
